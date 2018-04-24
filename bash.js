cb4d452bc4c1434ca1f1bd3462723e553bfb0f7c
AndrewTrahan <globalART19@gmail.com>
2018-04-24 11:11:36 -0500

    implemented switch and pwd command

---
+++ bash.js
@@ -0,0 +1,16 @@
+
+process.stdout.write('prompt > ')
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim()
+  let output
+  switch (cmd) {
+    case 'pwd':
+      output = process.cwd()
+      break
+    default:
+      output = 'command not recognized'
+      break
+  }
+  process.stdout.write(output)
+  process.stdout.write('\nprompt > ')
+})
